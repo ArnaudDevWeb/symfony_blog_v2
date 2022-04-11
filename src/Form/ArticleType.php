b2e2ac2113e786afa26fa21c406c9c6a7a92956f
ArnaudDevWeb <arnaud.albouycci@gmail.com>
2022-04-11 17:15:58 +0200

    Liaison category avec article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,14 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Validator\Constraints\Length;
 
 class ArticleType extends AbstractType
 {
@@ -25,10 +27,13 @@ class ArticleType extends AbstractType
                     new Length([
                         'min' => 2,
                         'max' => 75,
-                        'minMessage' => 'Le titre doit contenir au moins {{ limit }} caractères.',
-                        'minMessage' => 'Le titre doit contenir au maximum {{ limit }} caractères.'
+                        'minMessage' => 'Le titre doit contenir au moins {{ min }} caractères.',
+                        'minMessage' => 'Le titre doit contenir au maximum {{ max }} caractères.'
                     ])
                 ]
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
             ])
             // NotBlank, lengh mini a 100
             ->add('content', TextareaType::class, [
@@ -38,7 +43,7 @@ class ArticleType extends AbstractType
                     ]),
                     new Length([
                         'min' => 50,
-                        'minMessage' => 'Votre contenu doit au moins contenir {{100}} caractères.'
+                        'minMessage' => "Votre contenu doit au moins contenir {{ limit }} caractères."
                     ])
                 ]
             ]) 
